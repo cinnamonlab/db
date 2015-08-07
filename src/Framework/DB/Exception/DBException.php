3bf564ad13983492fa44a3088297637465bbc1d9
hajime3333out <hotta@fastbid.asia>
2015-08-08 00:00:56 +0900

    Simplify Exception

--- src/Framework/DB/Exception/DBException.php
+++ src/Framework/DB/Exception/DBException.php
@@ -4,9 +4,4 @@ namespace Framework\DB\Exception;
 use Framework\Exception\FrameworkException;
 
 class DBException extends FrameworkException {
-
-    function __construct( $message, $code = 500 ) {
-        $this->code = $code;
-    }
-
 }
